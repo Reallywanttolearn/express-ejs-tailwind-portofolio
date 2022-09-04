f1253b01998e179c8de4527a5aa241a964882c40
themas febrianto <themasfebrianto13@gmail.com>
2022-09-04 15:08:06 +0700

    portofolio 70%

---
+++ public/javascript/onclick.js
@@ -0,0 +1,8 @@
+function skills() {
+    document.getElementById('skills').style.display = "block";
+    document.getElementById('portofolio').style.display = "none";
+}
+function portofolio() {
+    document.getElementById('skills').style.display = "none";
+    document.getElementById('portofolio').style.display = "block";
+}
